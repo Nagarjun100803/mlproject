041acce61f7535f3583daf3df548171f82c36ce1
Nagarjun <nagarjunramakrishnan10@gmail.com>
2024-01-02 11:43:54 +0530

    importing proper logging module

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 
@@ -20,7 +20,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
 
         
             
